feb24386da6b8bf1e8f4ee74f25f7dd8661ffc21
Elko-Lemiso <elkovandongen@gmail.com>
2020-12-09 11:12:38 +0100

    Vercel commit

--- client-config.js
+++ client-config.js
@@ -1,6 +1,6 @@
 const clientConfig = {
-  productImagePlaceholder: 'https://via.placeholder.com/434', // e.g https://via.placeholder.com/434 - Placeholder image URL for index page
-  singleImagePlaceholder: 'https://via.placeholder.com/200', // e.g https://via.placeholder.com/200 - Placeholder image URL for individual product page
+  productImagePlaceholder: 'https://laura.relevantonline.eu/wp-content/uploads/2020/09/download.png', // e.g https://via.placeholder.com/434 - Placeholder image URL for index page
+  singleImagePlaceholder: 'https://laura.relevantonline.eu/wp-content/uploads/2020/09/download.png', // e.g https://via.placeholder.com/200 - Placeholder image URL for individual product page
 };
 
 export default clientConfig;
